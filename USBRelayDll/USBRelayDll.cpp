dd58a62bda742af43284213d31e0cefecaff73b8
yiliuchuifeng <847745261@qq.com>
2015-11-10 09:42:07 +0800

    格式更改-江绍侠

--- USBRelayDll/USBRelayDll.cpp
+++ USBRelayDll/USBRelayDll.cpp
@@ -14,114 +14,37 @@ struct usb_relay_device_info *DeviceList;
 std::map<std::wstring, struct usb_relay_device_info*> m_mapIndexToDevice;
 
 std::map<int, std::wstring> mapNumToSeria;
+
 int state = 0;//0--主界面 1--输入设备序列号 2--输入继电器序号
+
 string inputbuffer;//输入寄存
 
-int result;
-int hd;
+int result; // 
+int hd; // 
 string Open_Close;//打开，关闭状态
 
+// 继电器初始化
+JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SystemInit(JNIEnv *, jclass){
 
-JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SystemInit
-(JNIEnv *, jclass)
-{
-	return usb_relay_init();
+	return usb_relay_init();  // 继电器初始化
 	cout << "SystemInit" << endl;
 
 }
-JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SetDeviceSerialNum
-(JNIEnv *env, jclass, jstring SerialNum)
-{
-	cout << "|               请输入设备序列号                 |" << endl;
-	//getline(cin, inputbuffer);
-	//strcpy_s(serial_number, inputbuffer.c_str());
-	const char *serial_number = env ->GetStringUTFChars(SerialNum,NULL);	 	
-	hd = usb_relay_device_open_with_serial_number(serial_number, strlen(serial_number));
-	cout << serial_number << endl;
-	return hd;
-}
 
-JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SetRelayNum
-(JNIEnv *, jclass, jint relay)
-{
-	cout << "|           请输入需要操作的继电器序号           |" << endl;
-	//getline(cin, inputbuffer);
-	//int relay_index;
-	//relay_index = atoi(inputbuffer.c_str());
-	return relay;
-}
-JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_OpenRelay
-(JNIEnv *, jclass, jint hd, jint relay_index)
-{
-	if ((relay_index > 0) && (relay_index < 255))
-	{
-		result = usb_relay_device_open_one_relay_channel(hd, relay_index);
-		if (result == 0)
-			cout << "|        已打开" << relay_index << "号继电器           |" << endl;
-		else
-			cout << "|                   操作失败                      |" << endl;
-	}
-	else if (relay_index == 255)
-	{
-		result = usb_relay_device_open_all_relay_channel(hd);
-		if (result == 0)
-			cout << "|               已打开全部继电器                 |" << endl;
-		else
-			cout << "|                   操作失败                    |" << endl;
-	}
-	else
-	{
-		cout << "|       继电器序号必须是0-255之间的整数          |" << endl;
-		cout << "|                   请重新输入..                 |" << endl;
-		result = 1;
-	}
-	return result;
-}
-
-JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_CloseRelay
-(JNIEnv *, jclass, jint hd, jint relay_index)
-{
-	if ((relay_index > 0) && (relay_index < 255))
-	{
-		result = usb_relay_device_close_one_relay_channel(hd, relay_index);
-		if (result == 0)
-			cout << "|        已关闭" << relay_index << "号继电器           |" << endl;
-		else
-			cout << "|                   操作失败                      |" << endl;
-	}
-	else if (relay_index == 255)
-	{
-		result = usb_relay_device_close_all_relay_channel(hd);
-		if (result == 0)
-			cout << "|               已关闭全部继电器                 |" << endl;
-		else
-			cout << "|                   操作失败                    |" << endl;
-	}
-	else
-	{
-		cout << "|       继电器序号必须是0-255之间的整数          |" << endl;
-		cout << "|                   请重新输入..                 |" << endl;
-		result = 1;
-	}
-	return result;
-}
-
-JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_FindDevice
-(JNIEnv *, jclass)
-{
-	int cur_device_num = 0;
+// 查找插入到电脑中的所有USB免驱继电器模块
+JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_FindDevice(JNIEnv *, jclass) {
 	
-	if (DeviceList)
-	{
+	int cur_device_num = 0;
+
+	if (DeviceList) {
 		usb_relay_device_free_enumerate(DeviceList);
 		DeviceList = NULL;
 	}
-	DeviceList = usb_relay_device_enumerate();
-	if (DeviceList)
-	{
+	DeviceList = usb_relay_device_enumerate(); // 查找插入到电脑中的所有USB免驱继电器模块
+
+	if (DeviceList) {
 		struct usb_relay_device_info *cur_device = DeviceList;
-		while (cur_device)
-		{
+		while (cur_device) {
 			cur_device_num++;
 			m_mapIndexToDevice[Ansi2WChar((char*)(cur_device->serial_number))] = cur_device;
 			mapNumToSeria[cur_device_num] = Ansi2WChar((char*)(cur_device->serial_number));
@@ -129,17 +52,15 @@ JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_FindDevice
 			cur_device = cur_device->next;
 		}
 		return cur_device_num;
-	}
-	else
-	{
+	} else {
 		cout << "|                    未找到设备                     |" << endl;
 		return 0;
 	}
 }
 
-JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SelectDevice
-(JNIEnv *, jclass, jint device)
-{
+// 选择指定的继电器设备 返回继电器设备句柄
+JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SelectDevice(JNIEnv *, jclass, jint device) {
+
 	cout << "|                      请选择设备：                       |" << endl;
 	//getline(cin, inputbuffer);
 	//int device_index = atoi(inputbuffer.c_str());
@@ -149,8 +70,76 @@ JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SelectDevice
 	return hd;
 }
 
-JNIEXPORT void JNICALL Java_indi_whc_webandcv_Java2Cpp_CloseDevice
-(JNIEnv *, jclass, jint device_h)
-{
-	 usb_relay_device_close(device_h);
+// 继电器命令 打开指定继电器开发
+JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_OpenRelay(JNIEnv *, jclass, jint hd, jint relay_index) {
+
+	if ((relay_index > 0) && (relay_index < 255)) {
+		result = usb_relay_device_open_one_relay_channel(hd, relay_index);
+		if (result == 0)
+			cout << "|        已打开" << relay_index << "号继电器           |" << endl;
+		else
+			cout << "|                   操作失败                      |" << endl;
+	} else if (relay_index == 255) { // 打开继电器上所有的控制开关
+
+		result = usb_relay_device_open_all_relay_channel(hd);
+		if (result == 0)
+			cout << "|               已打开全部继电器                 |" << endl;
+		else
+			cout << "|                   操作失败                    |" << endl;
+	} else {
+		cout << "|       继电器序号必须是0-255之间的整数          |" << endl;
+		cout << "|                   请重新输入..                 |" << endl;
+		result = 1;
+	}
+	return result;
 }
+// 继电器命令 关闭指定继电器开发
+JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_CloseRelay(JNIEnv *, jclass, jint hd, jint relay_index) {
+
+	if ((relay_index > 0) && (relay_index < 255)) {
+
+		result = usb_relay_device_close_one_relay_channel(hd, relay_index);
+		if (result == 0)
+			cout << "|        已关闭" << relay_index << "号继电器           |" << endl;
+		else
+			cout << "|                   操作失败                      |" << endl;
+	} else if (relay_index == 255) {
+		result = usb_relay_device_close_all_relay_channel(hd);
+		if (result == 0)
+			cout << "|               已关闭全部继电器                 |" << endl;
+		else
+			cout << "|                   操作失败                    |" << endl;
+	} else {
+		cout << "|       继电器序号必须是0-255之间的整数          |" << endl;
+		cout << "|                   请重新输入..                 |" << endl;
+		result = 1;
+	}
+	return result;
+}
+
+// 关闭继电器设备
+JNIEXPORT void JNICALL Java_indi_whc_webandcv_Java2Cpp_CloseDevice(JNIEnv *, jclass, jint hd) {
+	usb_relay_device_close(hd);
+}
+
+// 打开继电器设备 ，选择继电器设备<板子>
+JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SetDeviceSerialNum (JNIEnv *env, jclass, jstring SerialNum){
+	
+	cout << "|               请输入设备序列号                 |" << endl;
+	//getline(cin, inputbuffer);
+	//strcpy_s(serial_number, inputbuffer.c_str());
+	const char *serial_number = env ->GetStringUTFChars(SerialNum,NULL);	 	
+	hd = usb_relay_device_open_with_serial_number(serial_number, strlen(serial_number));
+	cout << serial_number << endl;
+	return hd;
+}
+
+JNIEXPORT jint JNICALL Java_indi_whc_webandcv_Java2Cpp_SetRelayNum (JNIEnv *, jclass, jint relay) {
+	
+	cout << "|           请输入需要操作的继电器序号           |" << endl;
+	//getline(cin, inputbuffer);
+	//int relay_index;
+	//relay_index = atoi(inputbuffer.c_str());
+	return relay;
+}
+
